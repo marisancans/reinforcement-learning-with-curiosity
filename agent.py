Softmax fixed

agent.py
@@ -403,10 +403,11 @@ class Agent(nn.Module):
         trans = torch.cat((state_t, next_state_t), dim=1)
         pred_action = self.inverse_model(trans)
 
-        #loss_inverse = -torch.log(pred_action)
-        #loss_inverse = loss_inverse.mean()
+        loss_inv = recorded_action_t * torch.log(pred_action)
+        loss_inverse = - loss_inv.mean()
+        
         mse = nn.MSELoss()
-        loss_inverse = mse(pred_action, recorded_action_t)
+        #loss_inverse = mse(pred_action, recorded_action_t)
 
         # --------------- FORWARD MODEL / CURIOSITY -------------------------
         cat_t = torch.cat((state_t, recorded_action_t), dim=1)